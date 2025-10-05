ed8493dfb28f3b014d66fc64e4cbc06362863882
Steven Mathew <mathewsteven92@gmail.com>
2025-10-05 16:46:20 -0400

    live quiz info page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
+import DefaultSEO from "@/components/DefaultSEO";
 import ProtectedRoute from "@/components/ProtectedRoute";
 import ChatWidget from "@/components/ChatWidget";
 import { Analytics } from "@vercel/analytics/next"
@@ -127,6 +128,7 @@ const App = () => {
     <HelmetProvider>
       <QueryClientProvider client={queryClient}>
         <TooltipProvider>
+          <DefaultSEO />
           <Toaster />
           <Sonner />
           <BrowserRouter>
